14c2375450bdd62fe32dce9286ad8050443e413e
Valerian <@VelValerian>
2025-11-24 05:06:04 +0200

    style(main): fix views.py after rename index.html to main.html

--- app_core/views.py
+++ app_core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from django.shortcuts import render
 
-def home(request):       return render(request, "pages/index.html")
+def home(request):       return render(request, "pages/main.html")
 def about(request):      return render(request, "pages/about.html")
 def corporate(request):  return render(request, "pages/corporate.html")
 def contacts(request):   return render(request, "pages/contacts.html")
