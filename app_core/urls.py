678a7d838b7151eeea8e824c666d6bdb064ec6bf
Valerian <@VelValerian>
2025-11-24 10:21:50 +0200

    refactor(main): add new urls and link to app_training

--- app_core/urls.py
+++ app_core/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = "core"
@@ -8,4 +8,6 @@ urlpatterns = [
     path("about/", views.about, name="about"),                  # Про Меня
     path("corporate/", views.corporate, name="corporate"),      # Корпоративные тренировки
     path("contacts/", views.contacts, name="contacts"),         # Контакты
+    path("trainings/", include(("app_trainings.urls", "trainings"), namespace="trainings")), # Тренировки
+
 ]
